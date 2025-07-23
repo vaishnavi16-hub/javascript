a0a90244110b63d4930340d0219cf37503c03d30
Your Name <your_email@example.com>
2025-07-23 17:15:38 +0530

    Solved multiple challenges using push , pop , index, update operatons etc.

---
+++ arraychallenge.js
@@ -0,0 +1,83 @@
+// 1. Declared and array named 'fruits' that contains the strings "mango","banana","orange".
+// Access the first element of the array and store it in a varible named"firstfruit"
+
+
+// let teaFlavors = ["green tea","black tea","coffee"];
+// const firsttea = teaFlavors[0];
+// console.log(firsttea);
+
+
+// let fruitflavors = ["mango","Apple","banana"];
+// const firstfruit = fruitflavors[1];
+// console.log(firstfruit);
+
+
+
+// 2. Declared an array named 'cities' containing "london ","tokiyo","paries" and "new york", Access the third element in the array and store it in a variable named 'favoritecity'.
+
+
+// let cities = ["london", "tokyo", "paris", "new york"];
+// const favoriteCity = cities[2];
+// console.log(favoriteCity); 
+
+// Output: tokyo
+
+
+
+// 3. You hsave an array named 'teatyped' cointaining "herbal tea","white tea", and "blacktea", change the second element of the array to "jasmine tea"
+
+// Declare the array first
+// let teatyped = ["herbal tea", "white tea", "black tea"];
+
+// Change the second element to "jasmine tea"
+// teatyped[1] = "jasmine tea";
+
+// Print the updated array
+// console.log(teatyped); // Output: ["herbal tea", "jasmine tea", "black tea"]
+
+
+// 4. Declare an array named 'citiesVisited' containing 'mumbai' and 'pune'. Add "add berline" to the array using the 'push' method. 
+
+
+// Example= 
+// let citiesVisited = ["mumbai", "pune"];
+// citiesVisited.push("berlin");
+// console.log(citiesVisited); 
+
+
+
+
+// 5.  you have an array named 'teaorder' with 'chai', 'iced tea', 'matcha' and 'early grey'. 
+// remove the last element of the array using the 'pop' method and store it in a variable named 'lastorder'
+
+// let teaOrders = ["chai","iced tea","matcha","earlgray"];
+
+// // Use PopStateEvent( and store the result in lastorder)
+// const lastOrder = teaOrders.pop();
+
+// console.log(teaOrders);
+// console.log(lastOrder);
+
+
+// Array PushMethod() methos
+// let a= [10,20,30,50]
+// a.push(60);
+// console.log(a);
+
+// output= [ 10, 20, 30, 50, 60 ]
+
+
+
+
+// Array popmethod()
+// let a = [20,39,48,89,34];
+// a.pop();
+// console.log(a)
+
+// output= [ 20, 39, 48, 89 ]
+
+
+
+
+
+
