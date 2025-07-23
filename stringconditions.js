5c594812ef56b643b3f1307c792046058e30933e
Your Name <your_email@example.com>
2025-07-23 11:44:07 +0530

    Added the examples and explantions  for == and != conditions for Strings

---
+++ stringconditions.js
@@ -0,0 +1,62 @@
+// Checking if the String is Equal to another String...
+// let username = "vaishnavi";
+// let anotherusername ="jadhav";
+
+// if (username == anotherusername){
+//     console.log("pick the another username");
+// } else {
+//     console.log("you can pick this username");
+// }
+
+// output:- 
+// you can pick this username
+
+
+// OR...
+// let username = "samarth";
+// let anotherusername = "college"; 
+
+
+
+// If your statement is true then.. ex pick another username
+// if (username != anotherusername ){
+//     console.log("pick another username");
+
+    // If your statement is false TouchEvent.apply. ex you can pick this username
+// }else{
+//     console.log("you can pickthis username");
+// }
+
+
+
+
+
+// The main difference between == ( Double Equals) and !=(Not Equals)
+
+// 1. ==( Double Equals):-  Use it when: You want to check if two values are different.
+
+
+
+// javascript
+// Copy
+// Edit
+// let age = 16;
+
+// if (age != 18) {
+//     console.log("You are not 18 years old.");
+// }
+
+
+
+// 2. != (Not Equals):- 
+//  Use it when: You want to check if two values are different.
+
+
+// javascript
+// Copy
+// Edit
+// let age = 16;
+
+// if (age != 18) {
+//     console.log("You are not 18 years old.");
+// }
