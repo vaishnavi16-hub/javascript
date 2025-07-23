3b0fb519b31b77156a3aaf5d1f698e0afb5eb035
Your Name <your_email@example.com>
2025-07-23 12:40:25 +0530

    Added the arryconditions with its examples

---
+++ arrayconditions.js
@@ -0,0 +1,30 @@
+// Checking if an array is empty or not:
+
+
+// let items = [];
+// console.log(items.length);
+
+// if(items.length === 0){
+//     console.log("Array is empty");
+// } else {
+//     console.log("Array is not empty");
+// }
+
+// output:- 0
+// Array ia empty
+
+// let items =["items"];
+// console.log(items.length);
+
+// if(items.lenght === 0){
+//     console.log("Array is empty");
+// } else {
+//     console.log("Array is not  empty");
+// }
+
+// output:- 1
+// Array is not empty
+
+
+
+
