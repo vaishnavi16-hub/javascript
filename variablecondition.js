579582bbc632efb4e44e6aad7425cf80a88e1704
Your Name <your_email@example.com>
2025-07-23 12:17:33 +0530

    Added the variables difference between number and  string of number with its example

---
+++ variablecondition.js
@@ -0,0 +1,52 @@
+// Checking if a variable is a number or not
+
+
+
+let score = "23";
+if (typeof score === "number"){
+    console.log("yes thisd is a number");
+} else{
+    console.log("no this is not a number");
+}
+
+// let score = 23;
+// if (typeof score === "number"){
+//     console.log("yes, this is number");
+// } else {
+//     console.log("no this is not a number");
+// }
+
+// output :- yes this is a number
+// 
+
+
+// 34 → Number
+// This is a numeric value
+
+// You can do math with it (add, subtract, etc.)
+
+// Type: number
+
+//  Example:
+
+// javascript
+// Copy
+// Edit
+// let a = 34;
+// console.log(typeof a); // number
+
+
+// "34" → String
+// This is text that looks like a number
+
+// You can’t directly do math on it
+
+// Type: string
+
+//  Example:
+
+// javascript
+// Copy
+// Edit
+// let b = "34";
+// console.log(typeof b); // string
