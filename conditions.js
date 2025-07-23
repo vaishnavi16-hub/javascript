2e6ab7e94041116fc0c4ccfafa6c16cf69d7fecc
Your Name <your_email@example.com>
2025-07-23 11:20:28 +0530

    solve conditions check using if-else in javascript

---
+++ conditions.js
@@ -0,0 +1,40 @@
+// // Checking if a number is greater than another number..
+
+// let num1 = 5
+// let num2 = 8
+
+// console.log("I am the regular upper code");
+
+// if (num1 > num2) {
+//     console.log("num 1 is greater than num2");
+// } else {
+//     console.log("No num1 is not gresater");
+// }
+
+// console.log("I am regular bottom code");
+
+
+//output:-
+// I ma regular upper Code
+// num1 is greater than num2
+// I ma regular bottom code
+
+
+
+let age1 = 16
+let age2 = 18
+
+console.log("This is the person age");
+
+if (age1 > age2){
+    console.log("thsi persons are Eligible for this scholarship");
+} else {
+ console.log("they was not eligible for any scholarship");
+}
+console.log("this is eligible but they cant available ");
+
+
+// op:- 
+// This is the person age
+// thsi persons are Eligible for this scholarship
+// this is eligible but they cant available
---
+++ primitives.js
@@ -0,0 +1,21 @@
+// This is primitive data types....
+
+// Numbers..
+let balance = 120;
+console.log(balance);
+console.log(typeof balance);
+
+
+
+// null and undefined
+
+// Null : null means it is their but theri values is not defined
+Example:- 
+
+
+let firstname = null;
+let lastname = undefined;
+console.log(firstname);
+console.log(lastname);
+
+
