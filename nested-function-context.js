a0d01f0540937bd5711363e4e0cbec906d5eeb35
Your Name <your_email@example.com>
2025-07-26 12:38:27 +0530

    Added the nested-function-context.js

---
+++ nested-function-context.js
@@ -0,0 +1,38 @@
+// NESTED FUNCTIONA AND CONTEXT
+
+// create an object person witha method introduce() that uses the 'this'  additionaly add properties of name & age that will result in history, my name is vaishnavi and i am 19.7 years old on calling introduce()
+
+
+
+// const person = {
+//   name: "Hitesh",
+//   age: 19.5,
+  
+//   introduce: function() {
+//     return `Hi, my name is ${this.name} and I am ${this.age} years old.`;
+//   }
+// };
+
+// console.log(person.introduce());
+
+// const person = {
+//     name: "vaishnavi",
+//     age: '12.5',
+
+//     introduce: function() {
+//         return 'hi, my name is ${this.name} and I am ${this.age} years old.';
+//     }
+// };
+// console.log(person.introduce());
+
+// 2. Write a function outer() that contains another function inner() and returns a value of 'Inner function called' on calling outer()
+
+
+// function outer() {
+//  function inner() {
+//     return "Inner function called";
+//  }
+//  return inner();
+// }
+
+// console.log(outer()); //Inner function called
