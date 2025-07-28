3459d9a92cef994da06cb40039e225bc70690c54
Your Name <your_email@example.com>
2025-07-28 17:16:00 +0530

    Added the oops-js folder with prototypes.js

---
+++ OOPS-JS/oop1.js
@@ -0,0 +1,32 @@
+// let car ={
+//     make : "toyota",
+//     model : "camry",
+//     year: 2020,
+//     start: function () {
+//         // Access properties..
+//         return `${this.make} car got started in ${this.year}`;
+//     },
+// }
+// console.log(car.start());
+
+
+// let bike = {
+//     make: "Honda",
+//     year: "2024",
+//     start: function () {
+//         return `${this.make} bike got started in ${this.year}`;
+//     }
+// };
+
+// console.log(bike.start());  //Honda bike got started in 2024
+
+
+
+// function Person(name ,age) {
+//     this.name = name
+//     this.age = age
+// }
+
+// let vaishnavi = new Person("vaishnavi gaikwad" , 20);
+// console.log(vaishnavi.name);// vaishnavi gaikwad
+// console.log(vaishnavi.age);// 20
