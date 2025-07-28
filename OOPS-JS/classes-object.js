40c15b5f932f34c921c29277e29cd37db2d0f5b3
Your Name <your_email@example.com>
2025-07-28 18:19:42 +0530

    Added the classes-object file

---
+++ OOPS-JS/classes-object.js
@@ -0,0 +1,46 @@
+// Create a class Vehicle with properties make and model, and a method getDetails() that returns a string "Make: [make], Model: [model]". Create a subclass Car that extends Vehicle and adds a method startEngine() that returns "Engine started".
+
+//prent class..
+// class Vehicle {
+//   constructor(make, model) {
+//     this.make = make;
+//     this.model = model;
+//   }
+
+//   getDetails() {
+//     return `Make: ${this.make}, Model: ${this.model}`;
+//   }
+// }
+
+// class Car extends Vehicle {
+//   startEngine() {
+//     return "Engine started";
+//   }
+// }
+
+// const myCar = new Car("Honda", "City");
+// console.log(myCar.getDetails());
+// console.log(myCar.startEngine());
+
+
+
+
+class Animal {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+    getDetails() {
+        return 'name: ${this.name}, gender:${this.gender}';
+    }
+}
+
+class Dog extends Animal {
+    startBark(){
+        return "Dog was barking";
+    }
+}
+
+const myDog = new Dog("moti","chiku");
+console.log(myDog.getDetails());
+console.log(myDog.startBark());// 
